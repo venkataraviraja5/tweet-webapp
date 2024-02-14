c4ea8f615ed37d034263130325d1553380a78e09
ravi raja <131418248+venkataraviraja5@users.noreply.github.com>
2024-02-14 11:04:42 +0530

    cloud

---
+++ server/utils/cloudinary.js
@@ -0,0 +1,9 @@
+const cloudinary = require('cloudinary').v2
+          
+cloudinary.config({ 
+  cloud_name: 'dzdfh2ehf', 
+  api_key: '936189442894761', 
+  api_secret: 'KkGaJPxkt715nyCC2WBImZY1eJA' 
+});
+
+module.exports = cloudinary
