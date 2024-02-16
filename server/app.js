7f7ae8044787ab6aaefd846d012699615765936e
ravi raja <131418248+venkataraviraja5@users.noreply.github.com>
2024-02-16 11:14:01 +0530

    changes

--- server/app.js
+++ server/app.js
@@ -38,5 +38,5 @@ app.listen(8080,()=>{
 })
 
 mongoose.connect(
-    ""
+    "mongodb+srv://raviraja2000:raviraja2000@cluster0.eabcdxj.mongodb.net/tweet?retryWrites=true&w=majority"
 ).then(() => console.log("connected")).catch((err) => console.log("not connected"))
